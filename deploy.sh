140d749b39a2bc1b7a2ab0b5fe9353bdaa7cf20c
Alexander Listratenkov <a.listratenkov@gmail.com>
2020-07-04 07:52:09 +0300

    add yandex metric

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync -avz -e "ssh -p 8192" --delete ./docs/.vuepress/dist/ root@srv01.flags8192.local:/var/www/dev.listratenkov.com/
+rsync -avz -e "ssh -p 8192" --delete ./docs/.vuepress/dist/ root@server.flags8192.local:/var/www/dev.listratenkov.com/
